77faea460517886efb64ff8ea863de925ccde333
a.vilgelm <a.vilgelm@tinkoff.ru>
2023-12-19 14:09:18 +0300

    fix

---
+++ internal/app/utils/utils_test.go
@@ -0,0 +1,19 @@
+package utils
+
+import "testing"
+
+func TestGenerateShortKey(t *testing.T) {
+	tests := []struct {
+		name string
+		want string
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := GenerateShortKey(); got != tt.want {
+				t.Errorf("GenerateShortKey() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
